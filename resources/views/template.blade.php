3f603361f06b337d93de01b42d53ba95e331d255
hyuwan <hermawanwahyu47023@gmail.com>
2021-07-04 22:24:51 +0700

    templating

---
+++ resources/views/template.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Dashboard</title>
+  @include('partials.css')
+  </head>
+<body class="hold-transition sidebar-mini layout-fixed">
+</body>
+  <div class="wrapper">
+  	@include('partials.navbar')
+
+  	@include('partials.sidebar')
+
+  	@yield('content')
+
+	@include('partials.js')
+  </div>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('','Home@index');
+
+/*Route::get('/', function () {
+    return view('admin/index');
+});*/
